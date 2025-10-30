cf1e30f65c5eaa4456efa13f5ef77a85455758cf
owyman2023-beep <o_wyman2023@coloradocollege.edu>
2025-10-30 13:51:19 -0600

    Update Halibut_data_graphs.py

--- Halibut_data_graphs.py
+++ Halibut_data_graphs.py
@@ -1,13 +1,28 @@
-import fun_process_data as fpd
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
-output1, output2 = fpd.process_data('/Users/owenwyman/Data/ev228_data/EDS_Data.csv', 'Fork length (cm)', 'Year')
+path_data = '/Users/owenwyman/Data/ev228_data/'
+file_name = 'EDS_3.csv'
+df = pd.read_csv(path_data + file_name)
+
+fork_length = df['Fork length (cm)']
+dates = df['Year']
+
+# average_fl = fork_length.mean(dim = 'Year')
+# print(average_fl)
+
+# df = pd.concat([output2, output1], axis=1)
+# print(df)
+# filtered_data = df[df['metANN'] != 999.90]
+# print(filtered_data)
+
+annual_mean = df.groupby('Year')['Fork length (cm)'].mean()
 
 
 fig, ax = plt.subplots(figsize=(12,6))
-ax.plot(filtered_data['Year'], filtered_data['Fork length (cm)'])
+ax.plot(annual_mean)
 ax.set_xlabel('Year')
-ax.set_ylabel('Annual Mean Temperature (°C)')
-ax.set_title('Annual Mean Temperature')
+ax.set_ylabel('Average Halibut Fork Length (cm)')
+ax.set_title('Halibut Fork Length Size from 2010-2024')
 plt.show()
