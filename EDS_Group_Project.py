0bf3b0c2bd5ba8e1411ed9a3a2a1adb45b38323f
eliholton7 <e_holton2023@coloradocollege.edu>
2025-11-04 10:12:17 -0700

    Descriptions added

--- EDS_Group_Project.py
+++ EDS_Group_Project.py
@@ -4,20 +4,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#Importing the data and creating a path for the figure to save to
 path_data = '/Users/eliholton/GitHub/Ev228-EDS-Group-Project/'
 file_name = 'IPHC-2024-FISS-Pacific-halibut_1998-2024b.csv'
 out_path = '/Users/eliholton/GitHub/Ev228-EDS-Group-Project/'
 out_name = 'Fork_Length.png'
+
+#Function to read the data and sort it into the desired groups.
 df = pd.read_csv(path_data+file_name)
 annual_mean = df.groupby('Year')['Fork length (cm)'].mean()
+
+#Creating an array of zeros in order to use the fill-between function in the graph.
 y1 = np.zeros(27)
 np.shape(y1)
 print(annual_mean)
 x_var = np.arange(1998,2025)
 #print(df)
 
-
+#Creating the plot of the data.
 fig, ax = plt.subplots(figsize=(12,6))
 ax.plot(annual_mean, linewidth = 2.5,)
 plt.rcParams['font.family'] = 'serif'
@@ -30,7 +34,3 @@ plt.ylim(81,95)
 plt.savefig(out_path+out_name)
 plt.show()
 
-
-#annual_mean2 = df.groupby('Year')['Net wt (kg)'].mean()
-#filtered_mean = annual_mean2.loc[1998:2024]
-#print(filtered_mean)
