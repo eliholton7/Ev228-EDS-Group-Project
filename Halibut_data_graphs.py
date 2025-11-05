b7b02123b2a0b30e4324f9e127f467615570bcec
owyman2023-beep <o_wyman2023@coloradocollege.edu>
2025-11-05 11:15:31 -0700

    Update Halibut_data_graphs.py

--- Halibut_data_graphs.py
+++ Halibut_data_graphs.py
@@ -9,6 +9,22 @@ df = pd.read_csv(path_data + file_name)
 fork_length = df['Fork length (cm)']
 dates = df['Year']
 
+annual_mean = df.groupby('Year')['Fork length (cm)'].mean()
+
+fig, ax = plt.subplots(figsize=(8,6))
+ax.bar(annual_mean.index, annual_mean.values, color='skyblue', edgecolor='darkblue', linewidth=2, width=0.8)
+ax.set_ylim(bottom=80, top=90)
+ax.set_xlabel('Year')
+ax.set_ylabel('Average Halibut Fork Length (cm)')
+ax.set_title('Pacific Halibut Fork Length Size from 2010-2024')
+plt.show()
+
+
+
+
+
+
+
 # average_fl = fork_length.mean(dim = 'Year')
 # print(average_fl)
 
@@ -16,13 +32,3 @@ dates = df['Year']
 # print(df)
 # filtered_data = df[df['metANN'] != 999.90]
 # print(filtered_data)
-
-annual_mean = df.groupby('Year')['Fork length (cm)'].mean()
-
-
-fig, ax = plt.subplots(figsize=(12,6))
-ax.plot(annual_mean)
-ax.set_xlabel('Year')
-ax.set_ylabel('Average Halibut Fork Length (cm)')
-ax.set_title('Halibut Fork Length Size from 2010-2024')
-plt.show()
